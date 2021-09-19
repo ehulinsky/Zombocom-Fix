8edbe812db33c130ebc6b068637031ad03348056
Ethan Hulinsky <ethan.hulinsky@gmail.com>
2021-09-18 21:32:44 -0500

    Refactor into functions and add comments

--- zombocom-fix-extension/content.js
+++ zombocom-fix-extension/content.js
@@ -1,41 +1,25 @@
+//used to halt execution for a time in milleseconds
+//usage: `await sleep(1000)`
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+//find the width of a div of class 'signup' when displaying string
+//this function is slightly problematic because it adds an element to the DOM every time it is called (but we only use it once right now)
+function getTextWidth(string) {
+    ghostText = document.createElement("div")
+    ghostText.classList.add('signup')
+    ghostText.style.position = "absolute"
+    ghostText.style.visibility = "hidden"
+    ghostText.style.height = "auto"
+    ghostText.style.width = "auto"
+    ghostText.style.whiteSpace = "nowrap"
+    ghostText.innerHTML = signupMessage
+    document.querySelector("body").appendChild(ghostText)
+    return ghostText.getBoundingClientRect().width
+}
 
-button = document.querySelector("#button");
-audio = document.querySelector("audio");
-imageDiv = document.getElementsByClassName("animate-flicker")[0]
-rotatingImage = document.getElementsByClassName("rotate")[0]
-
-
-signupMessage="Sign Up For The NewZLetter"
-
-
-
-ghostText = document.createElement("div")
-ghostText.classList.add('signup')
-ghostText.style.position = "absolute"
-ghostText.style.visibility = "hidden"
-ghostText.style.height = "auto"
-ghostText.style.width = "auto"
-ghostText.style.whiteSpace = "nowrap"
-ghostText.innerHTML = signupMessage
-document.querySelector("body").appendChild(ghostText)
-textWidth = ghostText.getBoundingClientRect().width
-
-text = document.createElement("div")
-text.classList.add('signup');
-text.style.display = "none"
-document.querySelector("body").appendChild(text)
-text.addEventListener("click",function(){window.open("https://zombo.com/join1.htm",'_self',false);})
-console.log(textWidth)
-text.style.marginLeft = (window.innerWidth/2 - textWidth/2)+"px"
-
-
-window.onresize = function() { text.style.marginLeft = (window.innerWidth/2 - textWidth/2)+"px" }
-
-//write a message one character at a time into the element
+//write a message one character at a time into element
 //prints capital Z as red
 async function typewrite(message,durationMs,element) {
     pauseTimeMs = durationMs/message.length
@@ -43,7 +27,7 @@ async function typewrite(message,durationMs,element) {
 
     for(i=0;i<message.length;i++) {
         if(message[i] === 'Z') {
-            element.innerHTML += "<div style='color:red;display:inline-block;'>"+message[i]+"</div>"
+            element.innerHTML += "<span style='color:red;'>"+message[i]+"</span>"
         }
         else {
             element.innerHTML += message[i]
@@ -53,7 +37,30 @@ async function typewrite(message,durationMs,element) {
     }
 }
 
-//play music, type out message, show message, and then restart
+
+button = document.querySelector("#button");
+audio = document.querySelector("audio");
+rotatingImage = document.getElementsByClassName("rotate")[0]
+
+
+signupMessage="Sign Up For The NewZLetter"
+textWidth = getTextWidth(signupMessage)
+
+
+//create newzletter link and add to DOM
+text = document.createElement("div")
+text.classList.add('signup');
+text.style.display = "none"
+document.querySelector("body").appendChild(text)
+text.addEventListener("click",function(){window.open("https://zombo.com/join1.htm",'_self',false);})
+text.style.marginLeft = (window.innerWidth/2 - textWidth/2)+"px"
+
+
+//recenter link when window is resized
+window.onresize = function() { text.style.marginLeft = (window.innerWidth/2 - textWidth/2)+"px" }
+
+//play music and type out message
+//103 second audio, 4 second wait, 1 second draw words, 3 second wait, restart = loop every 111 seconds
 async function playSequence() {
 
     audio.play()
@@ -72,20 +79,15 @@ async function playSequence() {
     text.style.display = "none"
 }
 
-
-
+//start loop when button is clicked
 button.addEventListener('click',function() {
     
     //remove button when clicked, so it is not sitting in the corner being distracting
     this.remove();
     
-    //audio loops by default but we need to pause at the end of it
+    //audio is set to loop in the html, but we need to pause at the end of it
     audio.loop = false
 
-    //103 second audio, 4 second wait, 1 second draw words, 3 second wait, restart = loop every 111 seconds
     playSequence()
     window.setInterval(playSequence, 111*1000)
 })
-
-
-
